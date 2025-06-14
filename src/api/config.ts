47db03fd735c637baba2cc54342bd2c93112e433
itsmetrina <trina.sikdar.2101@gmail.com>
2025-06-14 21:31:15 +0530

    dist

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,3 @@
 export const SPOTIFY_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const SPOTIFY_CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-export const SPOTIFY_CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
-export const SPOTIFY_REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI_PROD;
+export const SPOTIFY_REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
