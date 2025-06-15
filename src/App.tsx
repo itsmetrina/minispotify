04fc533615d42606dbf61dfd6f38cbe93fc3c2bb
itsmetrina <trina.sikdar.2101@gmail.com>
2025-06-15 21:39:00 +0530

    fixed route

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { useState, useEffect } from 'react'
-import { isLoggedIn } from './utils/auth';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router';
+import UserProfile from './blocks/UserProfile';
+import SecureRoutes from './utils/secureRoutes';
 
 const App = () => {
-	const [loggedIn, setLoggedIn] = useState(false);
-
-	useEffect(() => {
-		setLoggedIn(isLoggedIn());
-	}, []);
-
 	return (
-		<>
-			<Routes>
-				{loggedIn ? (
-					<>
-						<Route path="/home" element={<Home />} />
-					</>
-				) : (
-					<Route path="/login" element={<Login />} />
-				)}
-			</Routes>
-		</>
+		<Routes>
+			<Route path="/login" element={<Login />} />
+			<Route path="/" element={
+				<SecureRoutes>
+					<UserProfile />
+				</SecureRoutes>
+			} />
+			<Route path="/home" element={
+				<SecureRoutes>
+					<Home />
+				</SecureRoutes>
+			} />
+		</Routes>
 	)
 }
 
---
+++ src/utils/SecureRoutes.tsx
@@ -0,0 +1,17 @@
+import type { JSX } from "react";
+import { Navigate } from "react-router-dom";
+
+interface ChildProp {
+    children: JSX.Element;
+}
+
+const SecureRoutes = ({ children }: ChildProp) => {
+    const accessToken = sessionStorage.getItem('access_token');
+    
+    if (!accessToken) {
+        return <Navigate to="/login" replace={true} />
+    }
+    return children;
+}
+
+export default SecureRoutes
