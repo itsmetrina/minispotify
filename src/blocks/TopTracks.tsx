1363829c81fe880315bda48887493df07781d606
itsmetrina <trina.sikdar.2101@gmail.com>
2025-06-19 22:46:50 +0530

    fixing top tracks usage error

--- src/blocks/TopTracks.tsx
+++ src/blocks/TopTracks.tsx
@@ -1,44 +1,41 @@
-import { useState, useEffect } from "react";
-import { getTopTracks } from "../api/spotifyAPI";
-
-type Track = {
-    id: string;
-    name: string;
-};
+import { useEffect } from "react";
+import { useUserStore } from "../store/useUserStore";
 
 const TopTracks = () => {
-    const [tracks, setTracks] = useState<Track[]>([]);
+    // const [tracks, setTracks] = useState<Track[]>([]);
+    const { topTracks } = useUserStore();
 
     useEffect(() => {
-        fetchTopTracks();
+        // fetchTopTracks();
+        console.log(topTracks)
     }, []);
 
-    const fetchTopTracks = async () => {
-        const data = await getTopTracks();
-        console.log(data, 'data');
-        const topTracks = data.map((d: {
-            popularity: number;
-            duration_ms: number; name: string;
-        }) => {
-            return {
-                "track_name": d.name,
-                "track_popularity": d.popularity,
-                "track_length": d.duration_ms,
+    // const fetchTopTracks = async () => {
+    //     const data = await getTopTracks();
+    //     console.log(data, 'data');
+    //     const topTracks = data.map((d: {
+    //         popularity: number;
+    //         duration_ms: number; name: string;
+    //     }) => {
+    //         return {
+    //             "track_name": d.name,
+    //             "track_popularity": d.popularity,
+    //             "track_length": d.duration_ms,
 
-            }
-        });
-        console.log(topTracks, 'topTracks');
-        setTracks(data);
-    }
+    //         }
+    //     });
+    //     console.log(topTracks, 'topTracks');
+    //     setTracks(data);
+    // }
 
     return (
         <div className="flex items-center">
-            {tracks.map((track) => (
+            {/* {topTracks?.map((track) => (
                 <div key={track.id} className="flex items-center mr-4">
-                    {/* <img src={`https://i.scdn.co/image/${track.id}`} alt={track.name} className="w-10 h-10 rounded-full mr-2" /> */}
                     <span>{track.name}</span>
                 </div>
-            ))}
+            ))} */}
+            Top Tracks
         </div>
     );
 }
